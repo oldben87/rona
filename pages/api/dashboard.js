79d5cd959933591d9c66ede0d69f2321187440eb
Ben Webb <b-webb@outlook.com>
2020-09-25 23:52:47 +0100

    adding dashboard api

---
+++ pages/api/dashboard.js
@@ -0,0 +1,67 @@
+export default async function hello(req, res) {
+  const callRona =
+    "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure={%22date%22:%22date%22,%22newCases%22:%22newCasesByPublishDate%22,%20%22newTests%22:%22newTestsByPublishDate%22}"
+  try {
+    await fetch(callRona)
+      .then((response) => response.json())
+      .then((data) => {
+        const response = data.data
+        // reformat to populate null responses from fetch
+        const noNull = response.map((item) => {
+          let result_no_null = {
+            date: item.date.split("-").reverse().join(" - "),
+            newCases: item.newCases || 0,
+            newTests: item.newTests || 0,
+          }
+          return result_no_null
+        })
+        // calculate difference in days and add to reponse
+        const result = noNull.map((fill, index) => {
+          const testSevenDay =
+            noNull
+              .slice(index - 3, index + 3)
+              .map(tests)
+              .reduce((a, b) => a + b, 0) / 7
+
+          const caseSevenDay =
+            noNull
+              .slice(index - 3, index + 3)
+              .map(cases)
+              .reduce((a, b) => a + b, 0) / 7
+
+          return {
+            ...fill,
+            newCases: fill.newCases,
+            newTests: fill.newTests,
+            testSevenDay: testSevenDay.toFixed(1),
+            caseSevenDay: caseSevenDay.toFixed(1),
+          }
+        })
+        // send result of data
+        if (data.statusCode > 204) {
+          throw Error("Not good status")
+        } else {
+          res.send({ status: 200, data: result })
+        }
+      })
+      .catch(() =>
+        res.send({
+          status: 500,
+          data: { error: "No response from Corona API Server" },
+        })
+      )
+  } catch (err) {
+    res.send({
+      status: 500,
+      data: { error: "No response from server" },
+    })
+  }
+}
+
+export function cases(item) {
+  return item.newCases
+}
+
+export function tests(item) {
+  return item.newTests
+}

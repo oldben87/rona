fb39346d330ef8fccb520e87d2fbf41832ca2dc5
Ben Webb <ben.webb@candide.eu>
2020-10-13 22:14:00 +0100

    adds background back to component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from "react"
-import fetch from "isomorphic-unfetch"
-import { PageLayout, ErrorText } from "../src/components/common"
-import ChartSection from "../src/components/Dashboard/ChartSection"
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
+import { PageLayout, ErrorText } from '../src/components/common'
+import ChartSection from '../src/components/Dashboard/ChartSection'
 
 const Home = ({ data }) => {
   const { error } = data
@@ -16,39 +16,39 @@ const Home = ({ data }) => {
           <ChartSection
             chartTitle="Daily Case Numbers"
             main={data.map((item) => item.newCases)}
-            mainTitle={"New Cases"}
-            yTitle={"Cases"}
+            mainTitle={'New Cases'}
+            yTitle={'Cases'}
             baseLine={data.map((item) => item.caseSevenDay)}
-            baseLineTitle={"7 Day Average"}
+            baseLineTitle={'7 Day Average'}
             dates={dates}
             background="#fff5f5"
           />
           <ChartSection
             chartTitle="Daily Testing Numbers"
             main={data.map((item) => item.newTests)}
-            mainTitle={"New Tests"}
-            yTitle={"Tests"}
+            mainTitle={'New Tests'}
+            yTitle={'Tests'}
             baseLine={data.map((item) => item.testSevenDay)}
-            baseLineTitle={"7 Day Average"}
+            baseLineTitle={'7 Day Average'}
             dates={dates}
           />
           <ChartSection
             chartTitle="Covid-19 Death Figures"
             main={data.map((item) => item.newDeaths)}
-            mainTitle={"New Deaths"}
-            yTitle={"Deaths"}
+            mainTitle={'New Deaths'}
+            yTitle={'Deaths'}
             baseLine={data.map((item) => item.deathSevenDay)}
-            baseLineTitle={"7 Day Average"}
+            baseLineTitle={'7 Day Average'}
             dates={dates}
-            background="⁴"
+            background="#fff5f5"
           />
           <ChartSection
             chartTitle="Hospitals And Healthcare"
             main={data.map((item) => item.newAdmissions)}
-            mainTitle={"New Hospital Admissions"}
-            yTitle={"Patients"}
+            mainTitle={'New Hospital Admissions'}
+            yTitle={'Patients'}
             baseLine={data.map((item) => item.covidBeds)}
-            baseLineTitle={"Ventilator Beds"}
+            baseLineTitle={'Ventilator Beds'}
             thirdLine={data.map((item) => item.hospitalCases)}
             thirdLineTitle="Total In Hospital"
             dates={dates}
@@ -63,14 +63,14 @@ export default Home
 
 export async function getServerSideProps() {
   const headers = new Headers()
-  headers.append("Pragma", "no-cache")
-  headers.append("Cache-Control", "no-store, must-revalidate, no-cache")
-  headers.append("Expires", 0)
+  headers.append('Pragma', 'no-cache')
+  headers.append('Cache-Control', 'no-store, must-revalidate, no-cache')
+  headers.append('Expires', 0)
 
   const uri = `${
-    process.env.NODE_ENV === "production"
-      ? "https://rona-olive.vercel.app"
-      : "http://localhost:3000"
+    process.env.NODE_ENV === 'production'
+      ? 'https://rona-olive.vercel.app'
+      : 'http://localhost:3000'
   }/api/dashboard`
 
   const res = await fetch(uri, headers)

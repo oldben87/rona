14deadc080fbf9d4310682ed3e925a687f723f6f
Ben Webb <ben.webb@candide.eu>
2020-10-15 23:23:44 +0100

    update herd immunity

--- pages/herdImmunity.js
+++ pages/herdImmunity.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Divider } from '@chakra-ui/core'
+import { Flex, Grid, Text } from '@chakra-ui/core'
 import {
   PageLayout,
   PageSection,
@@ -20,7 +20,7 @@ const herdImmunity = ({ data }) => {
 
   const lastDay = data[0]
   const death =
-    lastDay.cumDeaths28DaysByDeathDate ||
+    lastDay.cumDeaths28DaysByPublishDate ||
     data.map(deaths).reduce((a, b) => a + b)
   const percent = ((death / lastDay.cumCasesByPublishDate) * 100).toFixed(2)
   const popDead = ((67886011 / 100) * percent).toFixed(0)
@@ -31,35 +31,6 @@ const herdImmunity = ({ data }) => {
       headerTitle="Herd Immunity"
     >
       <PageSection background="#fff5f5">
-        <Flex
-          flexGrow="1"
-          w={['100%', '100%', '100%', '50%']}
-          direction={['column', 'column', 'column', 'row']}
-        >
-          <TitleText>
-            What could be the impact of Herd Immunity to the UK?
-          </TitleText>
-        </Flex>
-        <Flex
-          flexGrow="1"
-          w={['100%', '100%', '100%', '50%']}
-          direction="column"
-          p={['0.25rem', '1rem', '1.8rem', '2.4rem']}
-        >
-          <TextRow>
-            The total of people who have been confirmed to have COVID-19 in the
-            UK is {lastDay.cumCasesByPublishDate.toLocaleString()}. There have
-            been {death.toLocaleString()} deaths.
-          </TextRow>
-          <TextRow>
-            This means that {percent}% of people die who have tested positive
-            for the COVID-19 Virus. At this current level there would be{' '}
-            {parseInt(popDead).toLocaleString()} people dead if everyone in the
-            UK contracted the virus.
-          </TextRow>
-        </Flex>
-      </PageSection>
-      <PageSection>
         <Flex
           flexGrow="1"
           w={['100%', '100%', '100%', '50%']}
@@ -82,16 +53,29 @@ const herdImmunity = ({ data }) => {
           </TextRow>
         </Flex>
       </PageSection>
-      <PageSection background="#fff5f5">
+      <PageSection>
         <Flex
           flexGrow="1"
           w={['100%', '100%', '100%', '50%']}
-          direction={['column', 'column', 'column', 'row']}
+          direction="column"
+          p={['0.25rem', '1rem', '1.8rem', '2.4rem']}
         >
-          <TitleText>
-            How much of the population needs to be immune for there to be herd
-            immunity?
+          <TitleText size="xl">
+            What could be the impact of Herd Immunity to the UK?
           </TitleText>
+          <Flex direction="column ">
+            <TextRow>
+              The total of people who have been confirmed to have COVID-19 in
+              the UK is {lastDay.cumCasesByPublishDate.toLocaleString()}. There
+              have been {death.toLocaleString()} deaths.
+            </TextRow>
+            <TextRow>
+              This means that {percent}% of people die who have tested positive
+              for the COVID-19 Virus. At this current level there would be{' '}
+              {parseInt(popDead).toLocaleString()} people dead if everyone in
+              the UK contracted the virus.
+            </TextRow>
+          </Flex>
         </Flex>
         <Flex
           flexGrow="1"
@@ -99,16 +83,109 @@ const herdImmunity = ({ data }) => {
           direction="column"
           p={['0.25rem', '1rem', '1.8rem', '2.4rem']}
         >
+          <TitleText size="xl">
+            How much of the population needs to be immune for there to be herd
+            immunity?
+          </TitleText>
           <TextRow>
-            <p>
+            <Text>
               Figures vary from as low as 43% to as high as 90%, with the higher
               number of 90% saved for viruses that are more contagious. And the
               lower figures for viruses that aren't as contagious. A general
               concensus has averaged at around 60% of our population would need
               to be immune, whether that be through a vaccine or by enough
               people contracting the virus through exposure.
-            </p>
+            </Text>
           </TextRow>
+        </Flex>
+      </PageSection>
+      <PageSection background="#fff5f5" direction="column">
+        <Flex
+          flexGrow="1"
+          w={['100%', '100%', '100%', '50%']}
+          direction={['column', 'column', 'column', 'row']}
+        >
+          <TitleText size="2xl">
+            How many deaths could there be if we reach the immunity number
+            without a vaccine?
+          </TitleText>
+        </Flex>
+        <Flex
+          flexGrow="1"
+          w={['100%', '100%', '100%', '50%']}
+          direction={['column', 'column', 'column', 'row']}
+          justify="center"
+        >
+          <Grid templateColumns="repeat(2, 1fr)" gap={3}>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              Herd Percentage
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              People Dead
+            </Text>
+
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {' '}
+              25%
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {Math.floor(popDead * 0.25).toLocaleString()}
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {' '}
+              43%
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {Math.floor(popDead * 0.43).toLocaleString()}
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {' '}
+              60%
+            </Text>
+            <Text
+              textAlign="center"
+              verticalAlign="middle"
+              bg="red.100"
+              p="5px"
+            >
+              {Math.floor(popDead * 0.6).toLocaleString()}
+            </Text>
+          </Grid>
         </Flex>
       </PageSection>
     </PageLayout>
@@ -128,6 +205,7 @@ export async function getServerSideProps() {
     cumCasesByPublishDate: 'cumCasesByPublishDate',
     cumDeaths28DaysByDeathDate: 'cumDeaths28DaysByDeathDate',
     newDeaths: 'newDeaths28DaysByPublishDate',
+    cumDeaths28DaysByPublishDate: 'cumDeaths28DaysByPublishDate',
   }
 
   const uriOverview =
--- src/components/common/TitleText.js
+++ src/components/common/TitleText.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, Divider } from '@chakra-ui/core'
 
-export function TitleText({ children }) {
+export function TitleText({ children, size = '3xl' }) {
   return (
     <>
-      <Text as="h2" p={2} fontSize="3xl" color="red.800" p="1rem">
+      <Text as="h2" p={2} fontSize={size} color="red.800" p="1rem">
         {children}
       </Text>
     </>

6a2b8ea1011cc76ba90939044d3a247ac01710d1
Ben Webb <ben.webb@candide.eu>
2020-10-17 23:34:39 +0100

    update the about page styling

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Flex, Text, Link } from '@chakra-ui/core'
-import { PageLayout, PageSection } from 'components/common'
+import { PageLayout, PageSection, TitleText } from 'components/common'
 
 export default function about() {
   return (
     <PageLayout tabTitle="COVID-19 UK About" headerTitle="About Our Mission">
-      <PageSection>
+      <PageSection background="#fff5f5">
         <Flex
           style={{
             margin: '0.7rem auto',
@@ -16,18 +16,18 @@ export default function about() {
             color: 'dimgray',
           }}
         >
-          <Text fontSize="1rem" fontStyle="italic">
+          <TitleText size="xl">
             We are aiming to provide a place, where people who have questions
             about the statistics, can ask questions using the data made publicly
             available.
-          </Text>
-          <Text fontSize="0.8rem" p="0.2rem">
+          </TitleText>
+          <Text p="0.2rem">
             We would like to use this website as a place of learning. A place to
             ask questions and see them answered using the data that we have
             available. I will do my utmost to remove any bias I may have, and
             just show and explain the data.
           </Text>
-          <Text fontSize="0.8rem" p="0.2rem">
+          <Text p="0.2rem">
             Please do get in contact if you have any questions you may have
             regarding available data, or if there is something you would like to
             know.
@@ -45,10 +45,8 @@ export default function about() {
             color: 'dimgray',
           }}
         >
-          <Text fontSize="1rem" fontStyle="italic">
-            Where do you get your data from?
-          </Text>
-          <Text fontSize="0.8rem" p="0.2rem">
+          <TitleText size="xl">Where do you get your data from?</TitleText>
+          <Text p="0.2rem">
             We currently are getting all our data straight from the government
             API.
           </Text>
